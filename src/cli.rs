8b67fb7dea022d6b3658100e79a0bab0a90bb18a
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-10-04 00:47:14 +0200

    style: cargo fmt

--- src/cli.rs
+++ src/cli.rs
@@ -6,27 +6,31 @@ pub fn build_cli() -> App<'static, 'static> {
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(Arg::with_name("verbose")
-            .short("v")
-            .long("verbose")
-            .help("Show each time tick on stdout")
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .help("Show each time tick on stdout"),
         )
-        .arg(Arg::with_name("starttime")
-            .required(true)
-            .value_name("STARTTIME")
-            .help("Start time of the Meeting. From then the remaining time is published")
+        .arg(
+            Arg::with_name("starttime")
+                .required(true)
+                .value_name("STARTTIME")
+                .help("Start time of the Meeting. From then the remaining time is published"),
         )
-        .arg(Arg::with_name("endtime")
-            .required(true)
-            .value_name("ENDTIME")
-            .help("End time of the Meeting. Until then the remaining time is published.")
+        .arg(
+            Arg::with_name("endtime")
+                .required(true)
+                .value_name("ENDTIME")
+                .help("End time of the Meeting. Until then the remaining time is published."),
         )
-        .arg(Arg::with_name("end text")
-            .long("end-text")
-            .value_name("STRING")
-            .takes_value(true)
-            .help("Text which is displayed when the countdown ends.")
-            .default_value("THE END \\o/")
+        .arg(
+            Arg::with_name("end text")
+                .long("end-text")
+                .value_name("STRING")
+                .takes_value(true)
+                .help("Text which is displayed when the countdown ends.")
+                .default_value("THE END \\o/"),
         )
 }
 
